3914c686711d5fbe008b06d36bc37389ed773dbb
Christia T <christiatidwell@bellsouth.net>
2018-04-28 01:24:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(names = ["Dopey", "Grumpy", "Bashful"])
+  
 end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
